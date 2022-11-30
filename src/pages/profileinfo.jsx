816f00cf85a79e9ccc1255fc27e29958faa67abd
JuanIgnacio <nachoureta2001@gmail.com>
2022-11-30 13:05:29 -0500

    cambios

--- src/pages/profileinfo.jsx
+++ src/pages/profileinfo.jsx
@@ -2,10 +2,62 @@ import React from "react"
 import Header from "../components/Header"
 import "bootstrap/dist/css/bootstrap.css"
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import { useEffect } from "react"
+import { RUTA_BACKEND } from "../conf"
+
+let usuario = JSON.parse(localStorage.getItem('usuario') || "[]")
 
 const ProfileInfo = () => {
 
     const navigate = useNavigate();
+
+    const [nombre,setNombre] = useState("");
+    const [apellido,setApellido] = useState("");
+    const [correo,setCorreo] = useState("");
+    const [direccion,setDireccion] = useState("");
+    const [departamento,setDepartamento] = useState("");
+    const [ciudad,setCiudad] = useState("");
+    const [cpostal,setCpostal] = useState("");
+    const [telefono,setTelefono] = useState("");
+
+    const actualizarDatos  = async () => {
+        const data = {
+            id : usuario.id,
+            nombre : nombre,
+            apellido : apellido,
+            correo : correo,
+            direccion : direccion,
+            departamento : departamento,
+            ciudad : ciudad,
+            codigo_postal : cpostal,
+            telefono : telefono
+        }
+
+        localStorage.setItem('usuario',JSON.stringify(data))
+
+        await fetch(`${RUTA_BACKEND}/actualizarDatos`,{
+            method : 'POST',
+            headers : {"Content-Type" : "application/json"},
+            body : JSON.stringify(data)
+        })
+    }
+
+    useEffect(() => {
+        setNombre(usuario.nombre)
+        setApellido(usuario.apellido)
+        setCorreo(usuario.correo)
+        setDireccion(usuario.direccion)
+        setDepartamento(usuario.departamento)
+        setCiudad(usuario.ciudad)
+        setCpostal(usuario.codigo_postal)
+        setTelefono(usuario.telefono)
+    }, [])
+
+    const logOut = () => {
+        localStorage.setItem('usuario',JSON.stringify([]))
+        navigate("/proyecto_g3_new/login")
+    }
 
     return <div className="bg-dark">
         <Header />
@@ -16,7 +68,7 @@ const ProfileInfo = () => {
                     <div class="d-grid gap-2">
                         <button type="button" className="btn btn-light btn-lg" style={{ width: "320px", height: "50px"}} onClick={()=>{navigate("/proyecto_g3_new/orderHistory")}}>Order History</button>
                         <button type="button" className="btn btn-light btn-lg" style={{ width: "320px", height: "50px"}}>Profile Info</button>
-                        <button type="button" className="btn btn-light btn-lg" style={{ width: "320px", height: "50px"}}>Log Out</button>
+                        <button type="button" className="btn btn-light btn-lg" style={{ width: "320px", height: "50px"}} onClick={()=>{logOut()}}>Log Out</button>
                     </div>
                 </div>
 
@@ -33,54 +85,54 @@ const ProfileInfo = () => {
                         <div className="row ">HOLA</div>
                         <div className="col-md-6">
                             <label className="form-label text-black">First Name</label>
-                            <input type="text" className="form-control"></input>
+                            <input type="text" className="form-control" value={nombre} onChange={(evt) => {setNombre(evt.target.value)}}></input>
                         </div>
 
                         <div className="col-md-6 text-black">
                             <label className="form-label ">Last Name</label>
-                            <input type="text" className="form-control"></input>
+                            <input type="text" className="form-control" value={apellido} onChange={(evt) => {setApellido(evt.target.value)}}></input>
                         </div>
 
                         <div className="col-12">
                             <label className="form-label text-black">Email</label>
-                            <input type="email" className="form-control"></input>
+                            <input type="email" className="form-control" value={correo} onChange={(evt) => {setCorreo(evt.target.value)}}></input>
                         </div>
 
                         <div className="col-12">
                             <label className="form-label text-black">Address</label>
-                            <input type="email" className="form-control"></input>
+                            <input type="text" className="form-control" value={direccion} onChange={(evt) => {setDireccion(evt.target.value)}}></input>
                         </div>
 
                         <div className="col-md-6">
                             <label className="form-label text-black">Aparmament, suit, etc</label>
-                            <input type="text" className="form-control"></input>
+                            <input type="text" className="form-control" value={departamento} onChange={(evt) => {setDepartamento(evt.target.value)}}></input>
                         </div>
 
                         <div className="col-md-6 text-black">
                             <label className="form-label">City</label>
-                            <input type="text" className="form-control"></input>
+                            <input type="text" className="form-control" value={ciudad} onChange={(evt) => {setCiudad(evt.target.value)}}></input>
                         </div>
 
                         <div className="col-md-6 text-black">
                             <label className="form-label">Country/Region</label>
                             <select className="form-select">
-                                <option selected>United States</option>
+                                <option selected>Peru</option>
                                 <option>...</option>
                             </select>
                         </div>
 
                         <div className="col-md-6 text-black">
                             <label className="form-label">Postal/zip code</label>
-                            <input type="text" className="form-control"></input>
+                            <input type="text" className="form-control" value={cpostal} onChange={(evt) => {setCpostal(evt.target.value)}}></input>
                         </div>
 
                         <div className="col-12 text-black">
                             <label className="form-label">Phone</label>
-                            <input type="tel" className="form-control"></input>
+                            <input type="tel" className="form-control" value={telefono} onChange={(evt) => {setTelefono(evt.target.value)}}></input>
                         </div>
 
                         <div className="col-md-4 text-black">
-                            <button type="submit" class="btn btn-primary" style={{ width: "200px", height: "40px" }}>Update Info</button>
+                            <button type="submit" class="btn btn-primary" style={{ width: "200px", height: "40px" }} onClick={actualizarDatos}>Update Info</button>
                         </div>
 
                         <div className="col-md-4">
