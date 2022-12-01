f54b29716b825f695d5c7a40541aa4cfb8a4bd4c
Geraldine191 <108587428+Geraldine191@users.noreply.github.com>
2022-11-30 23:15:46 -0500

    HISTORIAS GERAL TERMINADAS

--- src/pages/ratingusers.jsx
+++ src/pages/ratingusers.jsx
@@ -1,12 +1,27 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Header from "../components/Header"
 import "bootstrap/dist/css/bootstrap.css"
 import "../styles/H16.css"
+import { useState } from "react"
+import { RUTA_BACKEND } from "../conf"
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
   <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
 </svg>
 
 const Ratingusers = () => {
+    const [listaResenas, setlistaResenas] = useState([])
+
+    const httpObtenerResenas = async () => {
+        const resp = await fetch(`${RUTA_BACKEND}/resenas/usuarios`)
+        const data = await resp.json()
+        setlistaResenas(data)
+        console.log(data)
+    }
+
+    useEffect(() => {
+        httpObtenerResenas()
+    }, [])
+
     return <div className="bg-light">
         <Header />
         
@@ -25,28 +40,16 @@ const Ratingusers = () => {
                 </div> 
 
             </div>
-
-            <div className="container-fluid">
-                <p className="listatext" style={{ fontWeight:"bold" }}>Juan Lopez</p>
-                <p className="listasubtext">I completely recomend this service </p>
-            </div>
-
-            <hr style={{height:"20px"}}></hr>
-
-
-            <div className="container-fluid">
-                <di>  <p className="listatext" style={{ fontWeight:"bold" }}>Jhon Doe</p></di> 
-                <p className="listasubtext">Great Service</p>
-            </div>
-            
-            <hr style={{height:"20px"}}></hr>
-
-            <div className="container-fluid">
-                <div> <p className="listatext" style={{ fontWeight:"bold" }}>Carl Johnson</p> </div>
-                <p className="listasubtext">PC well builded and nice case quality</p>
-            </div>
-
-			<hr style={{height:"20px"}}></hr>
+            {
+                listaResenas.map((resena) => {
+                    return <div><div className="container-fluid">
+                    <p className="listatext" style={{ fontWeight:"bold" }}>{resena.usuario.nombre}</p>
+                    <p className="listasubtext">{resena.comentario}</p>
+                </div>
+                <hr style={{height:"20px"}}></hr>
+                </div>
+                })
+            }
           </div>
 
     </div>
--- src/pages/ticket.jsx
+++ src/pages/ticket.jsx
@@ -1,8 +1,43 @@
 import React from "react"
 import Header from "../components/Header"
 import "bootstrap/dist/css/bootstrap.css"
+import { useState } from "react"
+import { RUTA_BACKEND } from "../conf"
+import { useNavigate } from "react-router-dom"
+
+let usuario = JSON.parse(localStorage.getItem('usuario') || "[]")
 
 const Ticket = () => {
+
+    const navigate = useNavigate();
+
+    const [nombre,setNombre] = useState('');
+    const [correo,setCorreo] = useState('');
+    const [telefono,setTelefono] = useState('');
+    const [asunto,setAsunto] = useState('');
+    const [descripcion,setDescripcion] = useState('');
+
+    const enviarTicket = async () => {
+        const data = {
+            id : usuario.id,
+            correo : correo,
+            nombre : nombre,
+            telefono : telefono,
+            asunto : asunto,
+            descripcion : descripcion
+        }
+
+        if (usuario.id === null || usuario.id === undefined) {
+            navigate("/proyecto_g3_new/login")
+        } else {
+            await fetch(`${RUTA_BACKEND}/reporte/generar`,{
+                method : 'POST',
+                headers : {"Content-Type" : "application/json"},
+                body : JSON.stringify(data)
+            })
+        }
+    }
+
     return <div className="bg-dark">
         <Header />
         <div className="container" style={{ textAlign: "center" }}>
@@ -16,31 +51,31 @@ const Ticket = () => {
                 
                 <div className="col-md-12">
                     <label className="form-label">Email</label>
-                    <input type="text" className="form-control"></input>
+                    <input type="text" className="form-control" value={correo} onChange={ (evt) => setCorreo(evt.target.value) }></input>
                 </div>
 
                 <div className="col-md-12">
                     <label className="form-label">Name</label>
-                    <input type="text" className="form-control"></input>
+                    <input type="text" className="form-control" value={nombre} onChange={ (evt) => {setNombre(evt.target.value)} }></input>
                 </div>
 
                 <div className="col-12">
                     <label className="form-label">Phone</label>
-                    <input type="email" className="form-control"></input>
+                    <input type="text" className="form-control" value={telefono} onChange={ (evt) => {setTelefono(evt.target.value)} }></input>
                 </div>
 
                 <div className="col-12">
                     <label className="form-label">Subject</label>
-                    <input type="email" className="form-control"></input>
+                    <input type="text" className="form-control" value={asunto} onChange={ (evt) => {setAsunto(evt.target.value)} }></input>
                 </div>
 
                <div class="form-group">
-                    <label for="exampleFormControlDescription">Description</label>
+                    <label for="exampleFormControlDescription" value={descripcion} onChange={ (evt) => {setDescripcion(evt.target.value)} }>Description</label>
                     <textarea class="form-control" id="exampleFormControlDescription" rows="3"></textarea>
                 </div>
 
                 <div className="col-md-20" >
-                    <button type="submit" class="btn btn-primary" style={{ width: "160px", height: "40px", padding: "5px",textAlign: "center"}}>Submit</button>
+                    <button type="submit" class="btn btn-primary" style={{ width: "160px", height: "40px", padding: "5px",textAlign: "center"}} onClick={enviarTicket}>Submit</button>
                 </div>
 
             </form>
