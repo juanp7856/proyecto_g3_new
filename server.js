64e04a497c758d85e7e025a3c58b6244dcdc59b4
juanp7856 <quepasoamigitoxd@gmail.com>
2022-12-01 17:48:42 -0500

    final

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.get("/", (req, resp) => {
+app.get("/*", (req, resp) => {
     resp.sendFile(path.join(__dirname, 'build/index.html'))
 })
 
