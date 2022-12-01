0fc5c5132c33725297d2c0e2f7d7d0d5ca5888da
juanp7856 <quepasoamigitoxd@gmail.com>
2022-12-01 03:44:49 -0500

    ruta back

--- src/conf.js
+++ src/conf.js
@@ -1,6 +1,6 @@
-//const RUTA_BACKEND = "https://proyectog3backend-production.up.railway.app"
+const RUTA_BACKEND = "https://proyectog3backend-production.up.railway.app"
 const RUTA_IMG = "https://proyectog3backend-production.up.railway.app/images"
-const RUTA_BACKEND = "http://localhost:4444"
+//const RUTA_BACKEND = "http://localhost:4444"
 export {
     RUTA_BACKEND,
     RUTA_IMG
